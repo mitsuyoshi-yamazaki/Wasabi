79e9b10fa4fe1b93135d99e05d9dd4d7d4fee19f
mitsuyoshi-yamazaki <mitsuyoshi.tsukuba@gmail.com>
2019-02-16 23:43:58 +0900

    Create project

---
+++ Wasabi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wasabi
+//
+//  Created by 開発用 on 2019/02/16.
+//  Copyright © 2019 mitsuyoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
