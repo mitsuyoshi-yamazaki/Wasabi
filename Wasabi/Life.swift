fb7a6619c10588a8842ad380d3d9289b725fbbda
mitsuyoshi-yamazaki <mitsuyoshi.tsukuba@gmail.com>
2019-02-17 00:48:00 +0900

    Initialize lives and show as SKNode

---
+++ Wasabi/Life.swift
@@ -0,0 +1,46 @@
+//
+//  Life.swift
+//  Wasabi
+//
+//  Created by 開発用 on 2019/02/17.
+//  Copyright © 2019 mitsuyoshi. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Life {
+  enum Status {
+    case alive(energy: Double, node: SKNode)
+    case dead
+  }
+  
+  private(set) var status: Status
+  
+  var energy: Double {
+    switch status {
+    case let .alive(energy, _):
+      return  energy
+      
+    case .dead:
+      return 0.0
+    }
+  }
+  
+  init(position: CGPoint, energy: Double) {
+    let size = type(of: self).size
+    let shapeNode = SKShapeNode.init(ellipseOf: .init(width: size, height: size))
+    shapeNode.position = position
+    
+    let physicsBody = SKPhysicsBody.init(circleOfRadius: size / 2.0)
+    physicsBody.affectedByGravity = false
+    shapeNode.physicsBody = physicsBody
+    
+    status = .alive(energy: energy, node: shapeNode)    
+  }
+}
+
+extension Life {
+  static let size: CGFloat = 10.0
+  static let energy: Double = 10.0
+}
--- Wasabi/ViewController.swift
+++ Wasabi/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
   private var sceneView: SKView {
     return view as! SKView
   }
+  private var lives: [Life] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -25,13 +26,40 @@ class ViewController: UIViewController {
     let scene = SKScene.init(size: view.bounds.size)
     scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
     
-    let size: CGFloat = 10.0
-    let shapeNode = SKShapeNode.init(ellipseOf: .init(width: size, height: size))
-    let physicsBody = SKPhysicsBody.init(circleOfRadius: size / 2.0)
-    physicsBody.affectedByGravity = false
-    shapeNode.physicsBody = physicsBody
+    lives = createLives(in: view.bounds.size)
+    nodes(for: lives).forEach { scene.addChild($0) }
     
-    scene.addChild(shapeNode)
     sceneView.presentScene(scene)
   }
+  
+  private func createLives(in area: CGSize) -> [Life] {
+    return (0..<10)
+      .map { _ in
+        Life.init(position: randomPosition(in: area), energy: Life.energy)
+      }
+  }
+  
+  private func nodes(`for` lives: [Life]) -> [SKNode] {
+    return lives
+      .map { life -> SKNode? in
+        switch life.status {
+        case let .alive(_, node):
+          return node
+          
+        default:
+          return nil
+        }
+      }
+      .compactMap { $0 }
+  }
+}
+
+func randomPosition(in size: CGSize) -> CGPoint {
+  let widthRange = (0..<Int(size.width))
+  let x = Int.random(in: widthRange)
+  
+  let heightRange = (0..<Int(size.height))
+  let y = Int.random(in: heightRange)
+  
+  return CGPoint.init(x: x, y: y)
 }
